882d1aea3dc337a0da08fb2c929f8190db9b95b8
rralvesvarjao <rodriguesalves1975@uol.com.br>
2025-02-06 20:20:04 +0000

    Criei Desafio Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,10 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
+    int main(){
+        float p1, p2, p3, p4, ar1, ar2, ar3, ar4, po1, po2, po3, po4, pt1, pt2, pt3, pt4;
+        char a1, a2, a3, a4;
+    
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +20,51 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+
+    printf("Digite o nome da cidade 1: \n");
+    scanf("%s", &a1);
+    printf("Digite a Polução da cidade 1: \n");
+    scanf("%f", &po1);
+    printf("Digite a area cidade 1: \n");
+    scanf("%f", &ar1);
+    printf("Digite o PIB cidade 1 \n");
+    scanf("%f", &p1);
+    printf("Digite o Ponto Turistico cidade 1: \n");
+    scanf("%f", &pt1);
+    
+    printf("Digite o nome da cidade 2: \n");
+    scanf("%s", &a2);
+    printf("Digite a Polução da cidade 2: \n");
+    scanf("%f", &po2);
+    printf("Digite a area cidade 2: \n");
+    scanf("%f", &ar2);
+    printf("Digite o PIB cidade 2 \n");
+    scanf("%f", &p2);
+    printf("Digite o Ponto Turistico cidade 2: \n");
+    scanf("%f", &pt2);
+    
+    printf("Diigite o nome da cidade 3: \n");
+    scanf("%s", &a3);
+    printf("Digite a Polução da cidade 3: \n");
+    scanf("%f", &po3);
+    printf("Digite a area cidade 3: \n");
+    scanf("%f", &ar3);
+    printf("Digite o PIB cidade 3 \n");
+    scanf("%f", &p3);
+    printf("Digite o Ponto Turistico cidade 3: \n");
+    scanf("%f", &pt3);
+
+    printf("Digite o nome da cidade 4: \n");
+    scanf("%s", &a4);
+    printf("Digite a Polução da cidade 4: \n");
+    scanf("%f", &po4);
+    printf("Digite a area cidade 4: \n");
+    scanf("%f", &ar4);
+    printf("Digite o PIB cidade 4 \n");
+    scanf("%f", &p4);
+    printf("Digite o Ponto Turistico cidade 4: \n");
+    scanf("%f", &pt4);
 
     return 0;
 }
